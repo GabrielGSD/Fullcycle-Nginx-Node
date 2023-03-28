8caef86ffbeb46710b00bf9ddf5bbe048a9a0628
Gabriel de Souza <gabrieldaniel@gec.inatel.br>
2023-03-28 00:38:22 -0300

    Feat: add proxy reverse with nginx

--- Node/index.js
+++ Node/index.js
@@ -11,12 +11,50 @@ const config = {
 const mysql = require('mysql');
 const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('Gabriel')`;
+const sql = `
+  INSERT IGNORE INTO people (id, name)
+  SELECT * FROM (SELECT 2, 'Gabriel') AS tmp
+  WHERE NOT EXISTS (
+      SELECT id FROM people WHERE id = 2
+  ) LIMIT 1;
+`;
 connection.query(sql);
-connection.end();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Full Cycle Rocks!</h1>');
+  const sqlPeoples = `SELECT * FROM people`;
+  connection.query(sqlPeoples, (err, results) => {
+    if (err) {
+      console.error('Error fetching data from MySQL: ' + err.stack);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const html = `
+      <html>
+        <head>
+          <title>People Data</title>
+        </head>
+        <body>
+          <h1>Full Cycle Rocks!</h1>
+          <br />
+          <h3>People Data</h3>
+          <table>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+            </tr>
+            ${results.map((row) => `
+              <tr>
+                <td>${row.id}</td>
+                <td>${row.name}</td>
+              </tr>
+            `).join('')}
+          </table>
+        </body>
+      </html>
+    `;
+    res.send(html);
+  });
 });
 
 app.listen(port, () => {
